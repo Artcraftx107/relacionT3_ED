475f3f47322585ce8de2ea5d2a2354cab267a1ac
Artcraft <arturoao@uma.es>
2024-11-05 18:54:53 +0100

    Finished ListaArray and Lista

--- ejercicio1/src/main/java/tadsarray/ListaArray.java
+++ ejercicio1/src/main/java/tadsarray/ListaArray.java
@@ -1,4 +1,66 @@
 package tadsarray;
 
-public class ListaArray {
+import java.util.Arrays;
+import java.util.NoSuchElementException;
+
+public class ListaArray<E> {
+    private static final int INITIAL_CAPACITY = 10;
+    private E[] elements;
+    private int size;
+
+    public ListaArray(){
+        elements=(E[]) new Object[INITIAL_CAPACITY];
+        size=0;
+    }
+
+    //Metodo para agregar elemento al final de la lista
+    public void agregar(E item){
+        if(size== elements.length){
+            resize(elements.length*2);
+        }
+
+        elements[size++]=item;
+    }
+
+    //Metodo para quitar un elemento
+    public void quitar(E item){
+        if(isEmpty()){
+            throw new IllegalStateException("La lista esta vacia");
+        }
+        int pos = buscar(item);
+
+        //Desplaza los elementos a la izquierda
+        System.arraycopy(elements, pos+1, elements, pos, size - pos - 1);
+        elements[--size]=null;
+    }
+
+    //Metodo para conseguir la posicion de un elemento en la lista
+    private int buscar(E item) {
+        if (isEmpty()){
+            throw new IllegalStateException("La lista esta vacia");
+        }
+        boolean found = false;
+        int currentPos = 0;
+        while(currentPos<size && !found){
+            if(elements[currentPos].equals(item)){
+                found=true;
+            }else{
+                currentPos++;
+            }
+        }
+
+        //Si el elemento no esta en la lista, lanza excepcion
+        if(!found){
+            throw new NoSuchElementException("El elemento "+item+" no esta en la lista");
+        }
+        return currentPos;
+    }
+
+    private boolean isEmpty(){
+        return size==0;
+    }
+
+    private void resize(int nuevaCapacidad){
+        elements = Arrays.copyOf(elements, nuevaCapacidad);
+    }
 }
--- ejercicio1/src/main/java/tadsnoarray/Lista.java
+++ ejercicio1/src/main/java/tadsnoarray/Lista.java
@@ -1,4 +1,74 @@
 package tadsnoarray;
 
-public class Lista {
+import java.util.NoSuchElementException;
+
+public class Lista<E> {
+    //Clase interna que actua como Nodo
+    private static class Nodo<E>{
+        E dato;     //Dato del nodo
+        Nodo<E> siguiente; //Referencia al siguiente Nodo
+
+        Nodo(E dato){
+            this.dato=dato;
+            this.siguiente=null; //Inicialmente no apunta a ningun nodo
+        }
+    }
+
+    private Nodo<E> head;
+    private int size;
+
+    //Inicializamos lista vacia
+    public Lista(){
+        head=null;
+        size=0;
+    }
+
+    //Metodo para agregar elemento al final de la lista
+    public void agregarAlFinal(E item){
+        Nodo<E> nuevoNodo = new Nodo<>(item);
+
+        if(head==null){
+            //Lista vacia
+            head=nuevoNodo;
+        }else{
+            Nodo<E> actual = head;
+            //Recorre hasta ultimo nodo
+            while(actual.siguiente!=null){
+                actual=actual.siguiente;
+            }
+            actual.siguiente=nuevoNodo;
+        }
+        size++;
+    }
+
+    //Metodo para quitar elemento por valor
+    public boolean quitar(E item){
+        boolean found = false;
+        if(head == null){
+            throw new IllegalStateException("La lista esta vacia");
+        }
+
+        if(head.dato.equals(item)){
+            head=head.siguiente;
+            size--;
+        }else{
+            Nodo<E> actual = head;
+            while(actual.siguiente!=null && !found){
+                if(actual.dato.equals(item)){
+                    actual = actual.siguiente;
+                    size--;
+                    found=true;
+                }
+                actual=actual.siguiente;
+            }
+        }
+
+        //Si no lo encuentra, tira excepcion
+        if(!found){
+            throw new NoSuchElementException("El elemento "+item+" no esta en la lista");
+        }
+
+        //Si lo ecuentra, devuelve true para comunicar el elemento ha sido eliminado
+        return found;
+    }
 }
